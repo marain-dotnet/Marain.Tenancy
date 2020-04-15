9d63ebd7a6289fa0db76d71077cd48da7fcf05a2
Jonathan George <github@jonathangeorge.co.uk>
2020-04-15 22:39:35 +0100

    Add basic command line tool for tenant management. (#74)
    * Add basic command line tool for tenant management.
    
    * New specs and code fixes to support retrieving child tenants via the ClientTenantProvider when there are 0 or 1 children.
    
    * Additional CLI command to retrieve all info for a tenant
    
    * Support continuation tokens in list command
    
    * Disable packing for CLI
    
    * Reformatted classes to adhere to max line length to make things look nicer in GitHub.
    
    * Updated comments
    
    * Reset batch size and more comment tweaks for the List command.
    
    * Enable nullability, tidy up output writing and return types.
    
    * Make code more readable in List command.
    
    * Remove use of ForEach extension method.
    
    * Comment correction
    
    Co-authored-by: Jonathan George <Jonathan.George@endjin.com>

---
+++ Solutions/Marain.Tenancy.Cli/Marain/Tenancy/Cli/Commands/Create.cs
@@ -0,0 +1,70 @@
+﻿// <copyright file="Create.cs" company="Endjin Limited">
+// Copyright (c) Endjin Limited. All rights reserved.
+// </copyright>
+
+namespace Marain.Tenancy.Cli.Commands
+{
+    using System.Threading.Tasks;
+    using Corvus.Tenancy;
+    using McMaster.Extensions.CommandLineUtils;
+
+    /// <summary>
+    /// Creates a new tenant.
+    /// </summary>
+    [Command(Name = "create", Description = "Create a new tenant.")]
+    public class Create
+    {
+        private readonly ITenantProvider tenantProvider;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Create"/> class.
+        /// </summary>
+        /// <param name="tenantProvider">The tenant provider that will be used to create the new tenant.</param>
+        public Create(ITenantProvider tenantProvider)
+        {
+            this.tenantProvider = tenantProvider;
+        }
+
+        /// <summary>
+        /// Gets or sets the Id of the tenant that should be the parent of the new tenant.
+        /// </summary>
+        /// <remarks>
+        /// If ommitted, the tenant will be created at the top level, as a child of the root.
+        /// </remarks>
+        [Option(
+            CommandOptionType.SingleOrNoValue,
+            ShortName = "t",
+            LongName = "tenant",
+            Description = "The Id of the parent tenant. Omit if the child should be a parent of the root tenant.")]
+        public string? TenantId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the new tenant.
+        /// </summary>
+        [Option(
+            CommandOptionType.SingleValue,
+            ShortName = "n",
+            LongName = "name",
+            Description = "The name of the new tenant.")]
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// Executes the command.
+        /// </summary>
+        /// <param name="app">The current <c>CommandLineApplication</c>.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public async Task OnExecute(CommandLineApplication app)
+        {
+            if (string.IsNullOrEmpty(this.TenantId))
+            {
+                this.TenantId = this.tenantProvider.Root.Id;
+            }
+
+            ITenant child = await this.tenantProvider.CreateChildTenantAsync(this.TenantId).ConfigureAwait(false);
+            child.Properties.Set("name", this.Name);
+            await this.tenantProvider.UpdateTenantAsync(child).ConfigureAwait(false);
+
+            app.Out.WriteLine($"Created new child tenant with Id {child.Id} and name {this.Name}");
+        }
+    }
+}
---
+++ Solutions/Marain.Tenancy.Cli/Marain/Tenancy/Cli/Commands/Delete.cs
@@ -0,0 +1,60 @@
+﻿// <copyright file="Delete.cs" company="Endjin Limited">
+// Copyright (c) Endjin Limited. All rights reserved.
+// </copyright>
+
+namespace Marain.Tenancy.Cli.Commands
+{
+    using System.Threading.Tasks;
+    using Corvus.Tenancy;
+    using McMaster.Extensions.CommandLineUtils;
+
+    /// <summary>
+    /// Deletes a tenant.
+    /// </summary>
+    [Command(Name = "delete", Description = "Deletes a tenant.")]
+    public class Delete
+    {
+        private readonly ITenantProvider tenantProvider;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Delete"/> class.
+        /// </summary>
+        /// <param name="tenantProvider">The tenant provider that will be used to delete the tenant.</param>
+        public Delete(ITenantProvider tenantProvider)
+        {
+            this.tenantProvider = tenantProvider;
+        }
+
+        /// <summary>
+        /// Gets or sets the Id of the tenant to be deleted.
+        /// </summary>
+        [Option(
+            CommandOptionType.SingleValue,
+            ShortName = "t",
+            LongName = "tenant",
+            Description = "The Id of the parent tenant.")]
+        public string? TenantId { get; set; }
+
+        /// <summary>
+        /// Executes the command.
+        /// </summary>
+        /// <param name="app">The current <c>CommandLineApplication</c>.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public async Task OnExecute(CommandLineApplication app)
+        {
+            TenantCollectionResult children = await this.tenantProvider.GetChildrenAsync(this.TenantId, 1).ConfigureAwait(false);
+
+            if (children.Tenants.Count > 0)
+            {
+                app.Error.WriteLine(
+                    $"Cannot delete tenant with Id {this.TenantId} as it has children. Remove the child tenants first.");
+
+                return;
+            }
+
+            await this.tenantProvider.DeleteTenantAsync(this.TenantId).ConfigureAwait(false);
+
+            app.Out.WriteLine("Deleted tenant with Id " + this.TenantId);
+        }
+    }
+}
