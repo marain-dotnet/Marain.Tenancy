6cdf202a914a10c0fe7e9196fb0e228964882a13
James Dawson <james.dawson@endjin.com>
2020-05-12 22:38:08 +0100

    Deployment now ensures a default 'tenant admin' principle exists
    This principle is for use with the Marain CLI automationpredeploy
    The password for this principle is stored in the instance keyvault

--- Solutions/Marain.Tenancy.Deployment/Marain-PreDeploy.ps1
+++ Solutions/Marain.Tenancy.Deployment/Marain-PreDeploy.ps1
@@ -29,4 +29,39 @@ Function MarainDeployment([MarainServiceDeploymentContext] $ServiceDeploymentCon
         "Ability to read information about tenants",
         "TenancyReader",
         ("User", "Application"))
+
+
+    $keyVaultName = $ServiceDeploymentContext.InstanceContext.KeyVaultName
+    $tenantAdminSecretName = "DefaultTenantAdminPassword"
+    # Ensure a default tenancy administrator principal is setup
+    $defaultTenantAdminSpName = '{0}{1}tenantadmin' -f $ServiceDeploymentContext.InstanceContext.Prefix, $ServiceDeploymentContext.InstanceContext.EnvironmentSuffix
+    $existingSp = Get-AzADServicePrincipal -DisplayName $defaultTenantAdminSpName
+    if (!$existingSp) {
+        Write-Host "Setting-up default tenant administrator"
+        $newSp = & az ad sp create-for-rbac -n $defaultTenantAdminSpName --skip-assignment
+        if ($LASTEXITCODE -eq 0) {
+            $newSpObj = $newSp | ConvertFrom-Json
+            try {                
+                $ServiceDeploymentContext.InstanceContext.TenantAdminAppId = $newSpObj.appId
+                $ServiceDeploymentContext.InstanceContext.TenantAdminObjectId = (Get-AzADServicePrincipal -ApplicationId $newSpObj.appId).Id
+                $ServiceDeploymentContext.InstanceContext.TenantAdminSecret = $newSpObj.password
+                Set-AzKeyVaultSecret -VaultName $keyVaultName -Name $tenantAdminSecretName -SecretValue (ConvertTo-SecureString $newSpObj.password -AsPlainText -Force)
+            }
+            catch {
+                # we've not saved the secret yet and we'll lose it now, so get rid of the SP so we can try again next time
+                & az ad app delete --id $newSpObj.appId
+                throw $_
+            }
+        }
+        else {
+            Write-Error "The azure-cli returned non-zero status whilst creating the default tenant administrator principal - check previous errors"
+        }
+    }
+    else {
+        # Read the existing tenant admin details
+        $ServiceDeploymentContext.InstanceContext.TenantAdminAppId = $existingSp.ApplicationId
+        $ServiceDeploymentContext.InstanceContext.TenantAdminObjectId = (Get-AzADServicePrincipal -ApplicationId $existingSp.ApplicationId).Id
+        $ServiceDeploymentContext.InstanceContext.TenantAdminSecret = (Get-AzKeyVaultSecret -VaultName $keyVaultName -Name $tenantAdminSecretName).SecretValueText
+    }
+
 }
