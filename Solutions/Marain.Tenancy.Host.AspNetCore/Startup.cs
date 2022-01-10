b71cc319ba223082784ef42be556e384330f687d
Ian Griffiths <Ian.Griffiths@endjin.com>
2022-01-10 07:20:27 +0000

    Fix appsettings.Development.json
    Had wrong property names, and a spurious comma.
    
    Also added in swagger endpoint.

--- Solutions/Marain.Tenancy.Host.AspNetCore/Startup.cs
+++ Solutions/Marain.Tenancy.Host.AspNetCore/Startup.cs
@@ -1,7 +1,10 @@
 namespace Marain.Tenancy.Host.AspNetCore
 {
+    using System;
+
     using Corvus.Storage.Azure.BlobStorage;
 
+    using Menes;
     using Menes.Auditing.AuditLogSinks.Development;
     using Menes.Hosting.AspNetCore;
     using Microsoft.AspNetCore.Builder;
@@ -12,18 +15,21 @@ namespace Marain.Tenancy.Host.AspNetCore
 
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTenancyApiWithAspNetPipelineHosting();
+            services.AddTenancyApiWithAspNetPipelineHosting(ConfigureOpenApiHost);
             services.AddOpenApiAuditing();
 
-            var sp = services.BuildServiceProvider();
-
-            IConfiguration configuration = sp.GetRequiredService<IConfiguration>();
-
-            BlobContainerConfiguration rootStorageConfiguration = configuration
+            BlobContainerConfiguration rootStorageConfiguration = this.Configuration
                 .GetSection("RootBlobStorageConfiguration")
                 .Get<BlobContainerConfiguration>();
 
@@ -43,6 +49,25 @@ namespace Marain.Tenancy.Host.AspNetCore
             }
 
             app.UseMenesCatchAll();
+        }
+
+        // TODO: consolidate with functions startup code.
+        // This fixes a bug from that - the 2nd exception handler was wrong on two counts:
+        //  1. wrong exception type: if config is non-null and config.Documents is null, that's
+        //      not ArgumentNullException
+        //  2. wrong argument order: we had the nameof and message flipped
+        // In any case, this startup is likely to be needed by any host, so we should put it
+        // somewhere common.
+        private static void ConfigureOpenApiHost(IOpenApiHostConfiguration config)
+        {
+            ArgumentNullException.ThrowIfNull(config);
+
+            if (config.Documents == null)
+            {
+                throw new ArgumentException("AddTenancyApi callback: config.Documents", nameof(config));
+            }
+
+            config.Documents.AddSwaggerEndpoint();
         }
     }
 }
