6ef21a01ac419491055938313f7928141bfaca67
Howard van Rooijen <howard.vanrooijen@endjin.com>
2022-01-12 10:41:30 +0000

    aspnet hosting for tenancy (#376)
    * Add aspnet hosting
    Add docker compose support
    
    * Remove local.setting.json
    
    * Add missing target
    
    * Fix appsettings.Development.json
    
    Had wrong property names, and a spurious comma.
    
    Also added in swagger endpoint.
    
    * Remove appsettings.Development.json
    
    * Add appsettings.template.json with required structure for a functioning app.
    Exclude appsettings.development.json so that dev secrets don't get committed into the repo.
    
    * Prevent solution from being packaged as a .nupkg
    
    * Implement Matthew's suggestion
    
    Co-authored-by: Ian Griffiths <Ian.Griffiths@endjin.com>

---
+++ Solutions/Marain.Tenancy.Host.AspNetCore/Startup.cs
@@ -0,0 +1,73 @@
+namespace Marain.Tenancy.Host.AspNetCore
+{
+    using System;
+
+    using Corvus.Storage.Azure.BlobStorage;
+
+    using Menes;
+    using Menes.Auditing.AuditLogSinks.Development;
+    using Menes.Hosting.AspNetCore;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTenancyApiWithAspNetPipelineHosting(ConfigureOpenApiHost);
+            services.AddOpenApiAuditing();
+
+            BlobContainerConfiguration rootStorageConfiguration = this.Configuration
+                .GetSection("RootBlobStorageConfiguration")
+                .Get<BlobContainerConfiguration>();
+
+            services.AddTenantStoreOnAzureBlobStorage(rootStorageConfiguration);
+
+#if DEBUG
+            services.AddAuditLogSink<ConsoleAuditLogSink>();
+#endif
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMenesCatchAll();
+        }
+
+        // TODO: consolidate with functions startup code.
+        // This fixes a bug from that - the 2nd exception handler was wrong on two counts:
+        //  1. wrong exception type: if config is non-null and config.Documents is null, that's
+        //      not ArgumentNullException
+        //  2. wrong argument order: we had the nameof and message flipped
+        // In any case, this startup is likely to be needed by any host, so we should put it
+        // somewhere common.
+        private static void ConfigureOpenApiHost(IOpenApiHostConfiguration config)
+        {
+            ArgumentNullException.ThrowIfNull(config);
+
+            if (config.Documents is null)
+            {
+                throw new ArgumentException("AddTenancyApi callback: config.Documents", nameof(config));
+            }
+
+            config.Documents.AddSwaggerEndpoint();
+        }
+    }
+}
