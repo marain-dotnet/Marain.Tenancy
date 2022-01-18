57f429e27ceb55c96103deec0731f2aee42e2aea
Howard van Rooijen <Howard.vanRooijen@endjin.com>
2022-01-18 07:51:05 +0000

    Clean up

--- Solutions/Marain.Tenancy.DemoFrontEnd/Program.cs
+++ Solutions/Marain.Tenancy.DemoFrontEnd/Program.cs
@@ -1,30 +1,26 @@
 using Marain.Tenancy.Client;
 
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-TenancyClientOptions options = new TenancyClientOptions();
-options.TenancyServiceBaseUri = new Uri("http://host.docker.internal:53658/v1.0/invoke/tenancy/method");
-
-builder.Services.AddSingleton(options);
+builder.Services.AddSingleton(new TenancyClientOptions
+{
+    TenancyServiceBaseUri = new Uri("http://host.docker.internal:53658/v1.0/invoke/tenancy/method")
+});
 builder.Services.AddTenancyClient(enableResponseCaching:false);
-
 builder.Services.AddDaprClient();
 
-var app = builder.Build();
+WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
 }
+
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapRazorPages();
-
 app.Run();
